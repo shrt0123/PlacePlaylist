f06d102eb4fe7c5b2bfdeaf216574581191a4d43
shrt0123 <shrt0123@naver.com>
2023-12-10 05:44:13 +0900

    first service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [location, setLocation] = useState(null);
+  const [playlist, setPlaylist] = useState([]);
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        const dummyLocation = { id: 1, latitude: 37.7749, longitude: -122.4194 };
+        setLocation(dummyLocation);
+        getPlaylist(dummyLocation.id);
+      } catch (error) {
+        console.error('Error getting location:', error);
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  const getPlaylist = async (locationId) => {
+    try {
+      const response = await axios.get(`http://localhost:3001/playlists?locationId=${locationId}`);
+      setPlaylist(response.data);
+    } catch (error) {
+      console.error('Error getting playlist:', error);
+    }
+  };
+
+  const showSongs = (playlist) => {
+    setSelectedPlaylist(playlist);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>PP(Place Playlist)</h1>
+      {location && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your current location: {location.latitude}, {location.longitude}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      <h2>Playlist</h2>
+      <ul>
+        {playlist.map((playlist) => (
+          <li key={playlist.id} onClick={() => showSongs(playlist)}>
+            {playlist.title}
+          </li>
+        ))}
+      </ul>
+      {selectedPlaylist && (
+        <div>
+          <h2>Songs in {selectedPlaylist.title}</h2>
+          <ul>
+            {selectedPlaylist.songs.map((song) => (
+              <li key={song.id}>{`${song.title} by ${song.artist}`}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
